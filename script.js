d4fc1921ea5e5cc0e76874b595ddf53a704a04c1
Ayush Jain <jnayush15@gmail.com>
2022-09-21 21:01:37 +0530

    Introduced the Rolling dice fucntionality (Add dice to current score and switch to next player functionality is pending)

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-"use strict";
+'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
+
+// Starting conditions
+score0El.textContent = score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dic functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // Change later
+  } else {
+    // Switch to next player
+  }
+});
