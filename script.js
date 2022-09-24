bd57bc5733540644227b0451cf07013b030703ca
Ayush Jain <jnayush15@gmail.com>
2022-09-24 12:58:25 +0530

    Chages to reset functionality in progress.

--- script.js
+++ script.js
@@ -75,4 +75,11 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {});
+btnNew.addEventListener('click', function () {
+  score0El.textContent = score1El.textContent = 0;
+  current0El.textContent = current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+});
